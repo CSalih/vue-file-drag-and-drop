c918fb09127247a1fadc35abf0dd4d07c48ddf26
Salih Candir <salih.candir@styleflasher.at>
2021-11-03 17:57:26 +0100

    feat: add action
    Signed-off-by: Salih Candir <salih.candir@styleflasher.at>

--- src/stories/vue-file-drag-and-drop.stories.js
+++ src/stories/vue-file-drag-and-drop.stories.js
@@ -1,3 +1,5 @@
+import { action } from "@storybook/addon-actions";
+
 import "../assets/css/tailwind.css";
 import VueFileDragAndDrop from "../components/VueFileDragAndDrop";
 
@@ -5,11 +7,11 @@ export default {
   title: "VueFileDragAndDrop",
   component: VueFileDragAndDrop,
   argTypes: {
-    maxFilesAllowed: {},
     allowedMimeTypes: {
-      control: { type: "select" },
-      // TODO: this can be a list of all mime types but for testing we can use a select field
-      options: ["video/*", "image/*", "*"],
+      // TODO: for testing we can use multi select
+      control: { type: "multi-select" },
+      description: "List of mime types",
+      options: ["*", "image/*", "video/*"],
     },
   },
 };
@@ -17,13 +19,16 @@ export default {
 const Template = (args, { argTypes }) => ({
   components: { VueFileDragAndDrop },
   props: Object.keys(argTypes),
+  methods: {
+    onFileAppend: action("onFileAppend"),
+    onInput: action("onInput"),
+  },
   template:
-    '<vue-file-drag-and-drop v-bind="$props" @file-appended="file-append" />',
+    '<vue-file-drag-and-drop v-bind="$props" @file-append="onFileAppend" @input="onInput"/>',
 });
 
-export const Default = Template.bind({});
-// More on args: https://storybook.js.org/docs/vue/writing-stories/args
-Default.args = {
+export const Simple = Template.bind({});
+Simple.args = {
   allowMultiple: true,
   maxFilesAllowed: 10,
   allowedMimeTypes: ["*"],
